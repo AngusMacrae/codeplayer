5408317e0f3d425b6f0a00143a645cdd2acf2c5b
Angus Macrae <angusdmacrae@gmail.com>
2020-04-30 17:58:08 +0800

    Reduced tab toggling to a single function

--- script.js
+++ script.js
@@ -1,75 +1,11 @@
-let htmlVis = true;
-let cssVis = true;
-let jsVis = true;
-let outputVis = true;
-
 $("textarea").on('change keyup paste', function() {
     
     $("#output").contents().find("html").html($("#html-input").val());
     
 });
 
-
-$("#html-toggle").click(function() {
+$(".tab-toggle").click(function() {
     
-    if (htmlVis) {
-        
-        $("#html-panel").hide();
-        htmlVis = false;
-         
-    } else {
-        
-        $("#html-panel").show();
-        htmlVis = true;
-        
-    }
-    
-});
-
-$("#css-toggle").click(function() {
-    
-    if (cssVis) {
-        
-        $("#css-panel").hide();
-        cssVis = false;
-         
-    } else {
-        
-        $("#css-panel").show();
-        cssVis = true;
-
-    }
-    
-});
-
-$("#js-toggle").click(function() {
-    
-    if (jsVis) {
-        
-        $("#js-panel").hide();
-        jsVis = false;
-         
-    } else {
-        
-        $("#js-panel").show();
-        jsVis = true;
-        
-    }
-    
-});
-
-$("#output-toggle").click(function() {
-    
-    if (outputVis) {
-        
-        $("#output-panel").hide();
-        outputVis = false;
-         
-    } else {
-        
-        $("#output-panel").show();
-        outputVis = true;
-        
-    }
+    $("#" + $(this).attr("id").slice(0,-6) + "panel").toggle();
     
 });
