373df8e0b62bec4ef9f55f854433bd1c27a4d79a
Angus Macrae <angusdmacrae@gmail.com>
2020-05-01 11:30:32 +0800

    Styling improvements to top bar

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-$("textarea").on('change keyup paste', function() {
-    
-    $("#output").contents().find("html").html($("#html-input").val());
-    
-});
+updateOutput();
+
+$("textarea").on('change keyup paste', updateOutput);
 
 $(".tab-toggle").click(function() {
     
     $("#" + $(this).attr("id").slice(0,-6) + "panel").toggle();
     
 });
+
+function updateOutput() {
+    
+    $("#output").contents().find("html").html("<html><head><style type='text/css'>" + $("#css-input").val() + "</style></head><body>" + $("#html-input").val() + "</body></html>");
+    
+    document.getElementById("output").contentWindow.eval($("#js-input").val());
+    
+}
